9eb7bb9dcde0ab9bed7081d47d0e771f3f3d4828
Ryan <ryanpadasian@gmail.com>
2025-05-01 02:41:34 +0800

    REMEMBER TO REMOVE STDLIB FUNCS

--- src/main.c
+++ src/main.c
@@ -6,67 +6,11 @@
 /*   By: rpadasia <ryanpadasian@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 15:04:00 by rpadasia          #+#    #+#             */
-/*   Updated: 2025/04/30 22:16:59 by rpadasia         ###   ########.fr       */
+/*   Updated: 2025/05/01 02:40:45 by rpadasia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headerfile/so_long.h"
-
-// int	main(int argc, char *argv[])
-// {
-// 	t_window 	win = {0};
-//     t_data 		mlx_img = {0};
-// 	char		**map;
-
-// 	if (argc != 2)
-// 	{
-// 		ft_printf("Usage: %s <map.ber>\n", argv[0]);
-// 		return (1);
-// 	}
-// 	map = load_map(argv[1]);
-// 	if (!map)
-// 	{
-// 		ft_printf("Error, Doesn't Exist.\n");
-// 		return (1);
-// 	}
-// 	if (!all_reachable(map))
-// 	{
-// 		ft_printf("Error, Unreachables Detected.\n");
-// 		free_char_array(map);
-// 		return (1);
-// 	}
-// 	if (!(validate_map(map)))
-// 	{
-// 		ft_printf("Error, Invalid Configuration\n");
-// 		free_char_array(map);
-// 		return (1);
-// 	}
-// 	t_mapsize map_size = get_map_size(map);
-// 	int width = map_size.x * 50;
-// 	int height = map_size.y * 50;
-
-// 	win.map = map;
-// 	win.items_left = count_collectibles(map);
-// 	win.moves = 0;
-// 	win.mlx = mlx_init();
-// 	if (!win.mlx)
-// 		return (1);
-
-// 	win.window = mlx_new_window(win.mlx, width - 50, height, "bug.exe");
-// 	mlx_img.img = mlx_new_image(win.mlx, width - 50, height);
-// 	win.img = &mlx_img;
-
-// 	load_sprites(&win);
-// 	load_player_idle(&win);
-// 	make_map(map, &win, 50);
-// 	mlx_key_hook(win.window, drax, &win);
-// 	mlx_hook(win.window, 17, 0, x_button, &win);
-// 	mlx_loop_hook(win.mlx, update, &win);
-// 	mlx_loop(win.mlx);
-// 	free_char_array(map);
-// 	cleanup(&win);
-// 	return (0);
-// }
 
 int	check_args(int argc, char **argv)
 {
